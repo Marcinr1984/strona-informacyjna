896077c202240f4b87d1561967ef7c539350b817
Marcinr1984 <marcin.rundsztuk@gmail.com>
2025-06-08 10:35:46 +0200

    Update contact page placeholder

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -2,7 +2,7 @@ export default function KontaktPage() {
   return (
     <div className="p-10">
       <h1 className="text-3xl font-bold">Kontakt</h1>
-      <p className="mt-4">Tutaj znajdą się dane kontaktowe... test......</p>
+      <p className="mt-4">Dane kontaktowe wkrótce.</p>
     </div>
   );
 }
